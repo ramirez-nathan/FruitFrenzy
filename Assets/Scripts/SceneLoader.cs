e41e9ee6774d72a4cac81681c0d80e5120864bfb
Michael Carlin <mcarl042@ucr.edu>
2025-06-01 17:44:17 -0700

    Play and restart buttons now load "EvironmentIntegrationFixed"

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,10 +1,45 @@
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
+//public class SceneLoader : MonoBehaviour
+//{
+//    public void LoadSceneByName(string sceneName)
+//    {
+//        Debug.Log("Loading scene: " + sceneName);
+//        SceneManager.LoadScene(sceneName);
+//    }
+
+//    public void QuitGame()
+//    {
+//        Debug.Log("Quit button pressed!");
+//        Application.Quit();
+//    }
+//}
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.Management;
+using System.Collections;
+
 public class SceneLoader : MonoBehaviour
 {
     public void LoadSceneByName(string sceneName)
     {
-        Debug.Log("Loading scene: " + sceneName);
+        Debug.Log("Start loading scene: " + sceneName);
+        StartCoroutine(LoadSceneWithXR(sceneName));
+    }
+
+    IEnumerator LoadSceneWithXR(string sceneName)
+    {
+        yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+
+        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        {
+            Debug.LogError("XR loader failed to initialize!");
+            yield break;
+        }
+
+        XRGeneralSettings.Instance.Manager.StartSubsystems();
+        Debug.Log("XR initialized. Loading scene: " + sceneName);
         SceneManager.LoadScene(sceneName);
     }
 
